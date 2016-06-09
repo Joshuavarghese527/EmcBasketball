55d41aeea0a10e093b1c2fe8ec4d5c29378dd102
Joshua Varghese <joshuavarghese527@gmail.com>
2016-06-09 03:35:16 +0000

    compile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EmcBasketball::Application.config.secret_key_base = '6ae64cf5218bd4a99a2619f38b7c1027fa5d8f7b66c0269b28a119f0f871d98523e54a4bbd20e58df968855bff50d8516d0f82b26b1cab97b8a19db50b9e6970'
